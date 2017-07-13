03503f6602af9c0ae8a8aaed2972c8b94d43d29a
diablo <diablo@diablo-Latitude-E5520>
2017-07-13 20:16:37 +0530

    second commit

---
+++ src/com/diablo/newservlet.java
@@ -0,0 +1,40 @@
+package com.diablo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class newservlet
+ */
+@WebServlet("/newservlet")
+public class newservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String sts = request.getParameter("usrname1");
+		PrintWriter pw = response.getWriter();
+		
+		pw.println("hi from get "+sts);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String sts = request.getParameter("usrname");
+		PrintWriter pw = response.getWriter();
+		pw.println("hi from post "+sts);
+	}
+
+}
